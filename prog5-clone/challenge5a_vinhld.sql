b2d1c73007fbccde09d3ccc8f49f2f89fe3908c3
Vatvo69 <vinhlevatvo.102@gmail.com>
2022-06-01 23:48:28 +0700

    Done Prog5

---
+++ prog5-clone/challenge5a_vinhld.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Jun 01, 2022 at 06:48 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `challenge5a_vinhld`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
+  `idTeacher` int(11) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `file` varchar(100) DEFAULT NULL,
+  `date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id`, `idTeacher`, `title`, `content`, `file`, `date`) VALUES
+(6, 1, 'Bai1', 'test', '1654099292_flag.docx', '2022-06-01 23:01:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game`
+--
+
+CREATE TABLE `game` (
+  `id` int(11) NOT NULL,
+  `idTeacher` int(11) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `hint` varchar(100) DEFAULT NULL,
+  `file` varchar(100) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `game`
+--
+
+INSERT INTO `game` (`id`, `idTeacher`, `title`, `hint`, `file`, `date`) VALUES
+(6, 1, 'Game1', 'hint', 'solve.txt', '2022-06-01 23:44:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `sendId` int(11) DEFAULT NULL,
+  `recvId` int(11) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `sendId`, `recvId`, `content`, `author`) VALUES
+(11, 1, 4, 'Hello', NULL),
+(24, 2, 1, 'Xin Chao toi la teacher2', 'teacher2'),
+(25, 1, 1, 'Xin chao toi la teacher1', 'teacher1'),
+(27, 3, 1, 'Xin Chao toi la student1', 'student1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `submit`
+--
+
+CREATE TABLE `submit` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `idStudent` varchar(100) DEFAULT NULL,
+  `idExercise` varchar(100) DEFAULT NULL,
+  `studentName` varchar(100) DEFAULT NULL,
+  `file` varchar(100) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `submit`
+--
+
+INSERT INTO `submit` (`id`, `title`, `idStudent`, `idExercise`, `studentName`, `file`, `date`) VALUES
+(2, 'Giai Bai 1', '3', '1', 'student1', '1654093916_solve.txt', '2022-06-01 21:31:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `role` int(11) DEFAULT NULL,
+  `avatar` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `fullname`, `email`, `phone`, `role`, `avatar`) VALUES
+(1, 'teacher1', '123456a@A', 'Le Van A', 'a@teacher1.com', '0123456789', 1, '1652238384-anh.png'),
+(2, 'teacher2', '123456a@A', 'Le Van B', 'a@a.com', '123456789', 1, '1652236823-anh.png'),
+(3, 'student1', '123456a@A', 'Le Van C', 'a@a.com', '123456789', 0, NULL),
+(4, 'student2', '123456a@A', 'Le Van D', 'a@student2.com', '123456789', 0, '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `submit`
+--
+ALTER TABLE `submit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `game`
+--
+ALTER TABLE `game`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `submit`
+--
+ALTER TABLE `submit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
